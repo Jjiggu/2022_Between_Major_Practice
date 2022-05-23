74333f03ec16381c67d7c473b9231fa6dc1290fa
Jihun Kim <zh1229@naver.com>
2022-05-23 18:53:53 +0900

    add test file

---
+++ 김지훈/2588.py
@@ -0,0 +1,19 @@
+a =  int(input())
+b =  int(input())
+
+b1 = b//100
+b2 = (b - b1*100)//10
+b3 = b % 10
+result = a * b
+
+print(b3*a)
+print(b2*a)
+print(b1*a)
+print(result)
+
+a = int(input())
+b = int(input())
+print(a * (b % 10))
+print(a * ((b//10)%10))
+print(a * int(b/100))
+print(a*b)
